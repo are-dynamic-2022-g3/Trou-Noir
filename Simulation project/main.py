4a67835d760d2c8e7f38e31f3a18bb1b99b3038a
Yass <YorkParodie@gmail.com>
2022-03-23 14:16:12 +0100

    updated

--- Simulation project/main.py
+++ Simulation project/main.py
@@ -1,5 +1,6 @@
 from random import randint, random, randrange
 import pygame
+from pygame import gfxdraw
 import math
 from typing import List
 
@@ -30,25 +31,27 @@ def main():
   bodies:List[Body] = []
   run = True
   is_clicking = False
-  cursors_body:Body = Body(mass = 1e8)
+  cursors_body:Body = Body(mass = 1e15)
 
-  number_of_stars = 100
-  size_min, size_max = 2, 10
+  number_of_stars = 5
+  size_min, size_max = 1, 10
   vmin, vmax = 5, 20
 
   #Generate random stars with parameters below
   for _ in range(number_of_stars):
+    new_color = randint(127, 255), randint(127, 255), randint(127, 255)
     size = randint(size_min, size_max)
     b = Body(pos = Vector2(randint(size, SCREEN_WIDTH - size), randint(size, SCREEN_HEIGHT - size)), \
       vel = Vector2(random() * vmax + vmin, random() * vmax + vmin), \
-        mass=size*50000, \
-          size = size)
+        mass=size*5e12, \
+          size = size, \
+            color = new_color)
     bodies.append(b)
 
 
 
   while run:
-    clock.tick(60)
+    clock.tick(144)
 
     cursors_body.position = Vector2(pygame.mouse.get_pos())
 
@@ -61,9 +64,12 @@ def main():
       if event.type == pygame.MOUSEBUTTONUP:
         is_clicking = False
 
-    
+    fadein = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fadein = fadein.convert()
+    fadein.fill((0, 0, 0))
+    fadein.set_alpha(127)
+    window.blit(fadein, (0, 0))
 
-    window.fill((0, 0, 0))
 
     #Apply forces
     for b in bodies:
@@ -73,7 +79,12 @@ def main():
         if b != other:
           b.apply_force_toward(other)
 
+          #Fusion
           if b.distance(other) < (b.size + other.size)/30:
+            if b.size < other.size:
+              temp = b
+              other = b
+              b = temp
             b.position = (b.position + other.position)/2
             b.mass += other.mass
             b.size += other.size
@@ -86,12 +97,11 @@ def main():
       apply_edge(b)
 
       
-      b.update(.0000001)
+      b.update(UPDATE_RATE)
       
-    #draw all bodies
     for b in bodies:
-      pygame.draw.circle(window, b.color, b.position, b.size, 2)
-
+      gfxdraw.circle(window, int(b.position.x), int(b.position.y), int(b.size), b.color)
+      
     pygame.display.flip()
 
 
