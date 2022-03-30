b520005a3e45a172e3a5167197e1098ca5d7a1dc
Yass <YorkParodie@gmail.com>
2022-03-30 16:49:52 +0200

    operation fixed

--- Simulation project/operation.py
+++ Simulation project/operation.py
@@ -3,4 +3,6 @@ from constants import *
 def mean_color(color1, color2):
     r1, g1, b1 = color1
     r2, g2, b2 = color2
-    return (r1 + r2)//2, (g1, g2)//2, (b1 + b2)//2
+    final_color = ((r1 + r2)//2, (g1 + g2)//2, (b1 + b2)//2)
+    return final_color
+
